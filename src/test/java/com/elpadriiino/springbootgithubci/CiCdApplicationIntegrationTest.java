42ee86941b7336db6b66741c323817e5ec715967
Elvis Dagana <elvis.dagana@gmail.com>
2024-05-08 08:21:43 +0100

    Travis

---
+++ src/test/java/com/elpadriiino/springbootgithubci/CiCdApplicationIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.elpadriiino.springbootgithubci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CiCdApplicationIntegrationTest {
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
